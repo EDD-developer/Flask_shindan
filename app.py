34260416220f5258f9c04a552dacf792ccf6ce8b
Keisuke Nagashima <energyrevo21@gmail.com>
2018-07-14 18:19:43 +0900

    add navbar

--- app.py
+++ app.py
@@ -8,38 +8,44 @@ Created on Sat Jun 23 14:13:36 2018
 
 from flask import Flask, request, render_template, url_for, redirect
 import random as rnd
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
+Bootstrap(app)
 
-@app.route("/")
+@app.route('/')
 def index():
-    title = "エロ診断メーカー"
     message = "名前を入力するのじゃ"
-    
-    return render_template("index.html", title = title, message = message)
+    return render_template("index.html", message = message)
 
-@app.route("/result", methods=['GET', 'POST'])
+@app.route('/result', methods=['GET', 'POST'])
 def result():
-    
     if request.method == 'POST':
-        title = "診断結果"      
-        my_name = request.form["name"]
-        message = "これが{}の診断結果じゃ。ちなみにワシゃ500くらい。".format(my_name)
+        my_name = request.form['name']
+        message = 'これが{}の診断結果じゃ。ちなみにワシゃ500くらい。'.format(my_name)
         
-        results = ["貴様の精力は9000000000000じゃと!?    ...まじぱねえっす。",
+        results = ["貴様の精力は9000000000000じゃと!? 参った...",
                    "貴様の精力は-5353535353535353。赤マムシでも飲んで出直すんじゃな。",
                    "貴様の精力は530。ワシといい勝負じゃな。フォッフォッフォ。"]
                 
         my_result = rnd.choice(results)
 
-        return render_template("index.html", title = title, message = message, result = my_result)    
+        return render_template('result.html', message = message, result = my_result)    
  
     else:
-        # エラーなどでリダイレクトしたい場合はこんな感じで
         return redirect(url_for('index'))
 
+@app.route('/manual')
+def manual():
+    return render_template('manual.html')
+    
+@app.route('/about_us')
+def about_us():
+    return render_template('about_us.html') 
 
-
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
 
 if __name__== '__main__':
     app.run(debug = True)
