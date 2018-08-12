86afd9f73aa801a4205fbc0039d29bccaf4b3325
Keisuke Nagashima <energyrevo21@gmail.com>
2018-08-12 20:21:49 +0900

    first commit

--- app.py
+++ app.py
@@ -1,27 +1,22 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 23 14:13:36 2018
-
-@author: rte28
-"""
 
 from flask import Flask, request, render_template, url_for, redirect
 import random as rnd
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
-Bootstrap(app)
+bootstrap = Bootstrap(app)
 
-@app.route('/')
+@app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route('/result', methods=['GET', 'POST'])
+@app.route("/result", methods=["GET", "POST"])
 def result():
-    if request.method == 'POST':
-        my_name = request.form['name']
-        message = 'これが{}の診断結果じゃ。ちなみにワシゃ500くらい。'.format(my_name)
+    if request.method == "POST":
+        my_name = request.form["name"]
+        message = "これが{}の診断結果じゃ。ちなみにワシゃ500くらい。".format(my_name)
         
         results = ["貴様の精力は9000000000000じゃと!? 参った...",
                    "貴様の精力は-5353535353535353。赤マムシでも飲んで出直すんじゃな。",
@@ -29,19 +24,11 @@ def result():
                 
         my_result = rnd.choice(results)
 
-        return render_template('result.html', message = message, result = my_result)    
+        return render_template("result.html", message = message, result = my_result)    
  
     else:
-        return redirect(url_for('index'))
+        return redirect(url_for("index"))
    
-@app.route('/about_us')
-def about_us():
-    return render_template('about_us.html') 
-
-@app.route('/blog')
-def blog():
-    return render_template('blog.html')
-
-if __name__== '__main__':
+if __name__== "__main__":
     app.run(debug = True)
     
